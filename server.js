b1a4ff38bd47a9d9b6e9bcd676b4bd65b41c478d
skaynar578 <skaynar578@gmail.com>
2026-05-17 01:58:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+
+let ads = [];
+
+// REKLAM EKLE
+app.post("/add", (req,res)=>{
+    ads.push(req.body);
+    fs.writeFileSync("ads.json", JSON.stringify(ads));
+    res.send("eklendi");
+});
+
+// REKLAMLARI GETİR
+app.get("/ads",(req,res)=>{
+    res.json(ads);
+});
+
+app.listen(3000,()=>{
+    console.log("server çalışıyor");
+});
